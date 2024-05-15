c4fcc3645a9f9a3dfa0b6d9105bff749dda7e78e
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-05-15 14:54:03 -0600

    updated swe __init__.py

--- snowai/swe/__init__.py
+++ snowai/swe/__init__.py
@@ -2,9 +2,9 @@
 The :mod:`snowai.swe` module includes statistical and machine learning models for computing snow water equivalent.
 """
 
-from .statistical_models import HillSWE, SWE_Models
+from .statistical_models import HillSWE, StatisticalModels
 
 __all__ = [
     'HillSWE',
-    'SWE_Models'
+    'StatisticalModels'
 ]
