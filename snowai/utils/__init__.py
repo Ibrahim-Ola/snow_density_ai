533aeb7988a73d611e7dbd3d7a6fabf8ed602c81
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-05-15 13:16:29 -0600

    refactore __init__

--- snowai/utils/__init__.py
+++ snowai/utils/__init__.py
@@ -2,12 +2,11 @@
 The :mod:`snowai.utils` module includes various utilities.
 """
 
+
 from .conversion_utils import ConvertData
-from .xgboost_utils import validate_DOY
 
 
 
 __all__ = [
-    'ConvertData',
-    'validate_DOY'
+    'ConvertData'
 ]
