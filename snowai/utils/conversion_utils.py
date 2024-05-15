b5d9360d65c1deadb21b0356299d4f93d73a5b43
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-05-15 12:15:11 -0600

    refactored conversion utils

--- snowai/utils/conversion_utils.py
+++ snowai/utils/conversion_utils.py
@@ -11,19 +11,21 @@ Institution: Boise State University (CryoGars Lab)
 
 import os
 import gdown
-import argparse
 import datetime
 import rioxarray
 import numpy as np
 import pandas as pd
-import xarray as xr
 from pyproj import Transformer
-from platformdirs import user_cache_dir
+from ._other_utils import (
+    get_cache_path,
+    OutOfBoundsError,
+    datetime_to_WaterYear, 
+    ensure_raster_available,
+    datetime_to_SturmWaterYear, 
+)
 
 
-class OutOfBoundsError(Exception):
-    """Exception raised when coordinates are outside the raster bounds."""
-    pass
+
 
 class ConvertData:
 
@@ -34,91 +36,8 @@ class ConvertData:
     def __init__(self):
         pass
 
-    
-    def datetime_to_SturmWaterYear(self, date: datetime.datetime | pd.Timestamp, origin: int =10) -> int | float:
-        """
-        A function to convert a datetime or pandas Timestamp object to a day of year (DOY) number using Sturm et al. (2010) algorithm. 
-        DOI: https://doi.org/10.1175/2010JHM1202.1
-
-        Note: Sturm et al. (2010) algorithm runs from -92 (1 October) to +181 (30 June) and +182 for a leap year.
-        
-        Parameters:
-        ===========
-            * date (datetime.datetime | pd.Timestamp): The date to convert.
-        
-        Returns:
-        ========
-            * DOY (int | float): The day of the water year or np.nan for dates between July 1 and September 30.
-                                 The np.nan is specifically returned for dates that fall outside the defined operational range of the Sturm algorithm.
-        """
-        
-        # determine if the month is valid for the algorithm
-        if 7 <= date.month < 10:
-            return np.nan
-        
-        # Normalize date to UTC
-        if isinstance(date, pd.Timestamp):
-            if date.tzinfo is not None:
-                date = date.tz_convert('UTC')
-            else:
-                date = date.tz_localize('UTC')
-        else:
-            if date.tzinfo is not None:
-                date = date.astimezone(datetime.timezone.utc)
-            else:
-                date = date.replace(tzinfo=datetime.timezone.utc)
-        
-        # Determine the reference date
-        reference = datetime.datetime(date.year if date.month < origin else date.year + 1, 1, 1, tzinfo=datetime.timezone.utc)
-        
-        # Calculate the day of the year offset from the reference date
-        delta = date - reference
-
-        DOY = delta.days
-
-        # Handle the skipping of 0 by adjusting DOY accordingly
-        if DOY >= 0:
-            DOY += 1
-        
-        return DOY
-    
-
-    def datetime_to_WaterYear(self, date: datetime.datetime | pd.Timestamp, origin : int = 10) -> int:
-        """
-        A function to convert a datetime or pandas Timestamp object to a day of year number where the origin is DOY 1.
-        The origin defaults to October 1st.
-        
-        Parameters:
-        ===========
-            * date (datetime.datetime | pd.Timestamp): The date to convert.
-        
-        Returns:
-        ========
-            * DOY (int): The day of the water year.
-        """
-
-        # Normalize date to UTC
-        if isinstance(date, pd.Timestamp):
-            if date.tzinfo is not None:
-                date = date.tz_convert('UTC')
-            else:
-                date = date.tz_localize('UTC')
-        else:
-            if date.tzinfo is not None:
-                date = date.astimezone(datetime.timezone.utc)
-            else:
-                date = date.replace(tzinfo=datetime.timezone.utc)
-
-
-        # Determine the start of the water year
-        water_year_start = datetime.datetime(date.year if date.month >= origin else date.year - 1, origin, 1, tzinfo=datetime.timezone.utc)
-        
-        # Calculate the day of the year offset from October 1
-        DOY = (date - water_year_start).days + 1
-        
-        return DOY
-    
-    def date_to_DOY(self, date: datetime.datetime | pd.Timestamp, origin: int = 10, algorithm: str = "default") -> int | float:
+    @staticmethod
+    def date_to_DOY(date: datetime.datetime | pd.Timestamp, origin: int = 10, algorithm: str = "default") -> int | float:
         """
         A function to convert a datetime or pandas Timestamp object to a day of year (DOY) number.
         
@@ -137,10 +56,10 @@ class ConvertData:
             raise TypeError(f"Expected date to be a datetime.datetime or pd.Timestamp, got {type(date).__name__} instead.")
         
         if algorithm == "default":
-            return self.datetime_to_WaterYear(date, origin)
+            return datetime_to_WaterYear(date, origin)
         
         elif algorithm == "Sturm":
-            return self.datetime_to_SturmWaterYear(date, origin)
+            return datetime_to_SturmWaterYear(date, origin)
 
         else:
             raise ValueError("Invalid algorithm. Choose between 'default' and 'Sturm'.")
@@ -216,38 +135,6 @@ class ConvertData:
         return measurement_in_m
     
     @staticmethod
-    def get_cache_path(filename="SnowClass_NA_300m_10.0arcsec_2021_v01.0.nc"):
-        """
-        Returns the path to the cached file, ensuring the cache directory exists.
-        """
-        cache_dir = user_cache_dir(appname="CroGarsAI", appauthor="BoiseStateUniversity", opinion=False)
-        if not os.path.exists(cache_dir):
-            os.makedirs(cache_dir)
-        return os.path.join(cache_dir, filename)
-    
-
-    @staticmethod
-    def ensure_raster_available():
-        """
-        Ensures that the necessary raster file is available in the cache,
-        downloading it if necessary, but only after user consent.
-        """
-        cache_path = ConvertData.get_cache_path()
-        if not os.path.exists(cache_path):
-            print("Snow classification raster needs to be downloaded.")
-            print("This file is approximately 2 GB in size and will be stored at: {}".format(cache_path))
-            user_input = input("Do you want to proceed with the download? (yes/no): ")
-            if user_input.lower() == 'yes':
-                raster_url = "https://drive.google.com/file/d/1yhthVbkdBNm_pL5wl5YlwNaKN96iUGa8/view?usp=sharing" 
-                print("Downloading now...")
-                gdown.download(url=raster_url, output=cache_path, fuzzy=True, quiet=False)
-                print("Download complete.")
-            else:
-                print("Download aborted. The application may not function properly without the raster data.")
-
-
-    
-    @staticmethod
     def get_snow_class(lon: float, lat: float, raster: None) -> str:
         
         """
@@ -266,9 +153,9 @@ class ConvertData:
 
 
         if raster is None:
-            ConvertData.ensure_raster_available()
-            cache_path = ConvertData.get_cache_path()
-            raster = xr.open_dataarray(cache_path)
+            ensure_raster_available()
+            cache_path = get_cache_path()
+            raster = rioxarray.open_rasterio(cache_path)
         
         snow_class_dict=dict(
             zip(
