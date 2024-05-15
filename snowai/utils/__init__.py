4f6e1968e20dc5762ab30053e80699fb1cf4817a
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-05-15 14:50:34 -0600

    updated __init__ with clearning cache

--- snowai/utils/__init__.py
+++ snowai/utils/__init__.py
@@ -4,9 +4,11 @@ The :mod:`snowai.utils` module includes various utilities.
 
 
 from .conversion_utils import ConvertData
+from ._other_utils import clean_cache
 
 
 
 __all__ = [
-    'ConvertData'
+    'ConvertData',
+    'clean_cache'
 ]
