a34c120f5c004cdaac609542695773e9c9b3ebbd
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-05-20 12:51:58 -0600

    updated ML swe computation

--- snowai/swe/machine_learning_model.py
+++ snowai/swe/machine_learning_model.py
@@ -1,14 +1,30 @@
-
+import numpy as np
+import pandas as pd
 from ..density import MachineLearningDensity
 
 
 class MachineLearningSWE(MachineLearningDensity):
     
-    def __init__(self, snow_depth: float):
+    def __init__(self, return_type: str = 'numpy'):
         super().__init__()
 
-        self.snow_depth = snow_depth
+        self.return_type = return_type
 
-    def predict(self, input_data):
-        density=super().predict(input_data)  # Call the predict method from the superclass to get the density.
-        return density * self.snow_depth  # Return the density multiplied by the snow depth to get the SWE.
+    def predict(self, input_data: pd.DataFrame, **kwargs) -> np.ndarray | pd.Series:
+        """
+        A function to compute snow water equivalent using the machine learning model.
+        """
+        
+        # validate return type
+        if self.return_type.lower() not in ['numpy', 'pandas']:
+            raise ValueError("Unsupported return type. Choose either 'numpy' or 'pandas'.")
+        
+        # preprocess and predict
+        density_preds = super().predict(data=input_data, **kwargs)
+        snow_depth = input_data[kwargs.get('snow_depth')].to_numpy()
+
+        SWE=density_preds * snow_depth
+
+        if self.return_type.lower() == 'pandas':
+            return pd.Series(SWE, index=input_data.index)
+        return SWE
